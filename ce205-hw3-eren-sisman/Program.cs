fc62731ae4ac270975cb5800c926b28a6c72dfbb
ERENSISMAN61 <eren_sisman20@erdogan.edu.tr>
2022-12-13 14:32:43 +0300

    added

---
+++ ce205-hw3-eren-sisman/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ce205_hw3_eren_sisman
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MnFrm());
+        }
+    }
+}
